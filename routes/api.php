cad2b4c4388bbb819039590d3fdf3592ca573a70
Chester0629 <yfwu0629@gmail.com>
2025-11-27 19:45:49 +0800

    Fix login route name and add API reference

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
