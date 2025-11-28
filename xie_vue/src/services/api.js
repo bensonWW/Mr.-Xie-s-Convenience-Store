fb9c7c7541850874d41b5cae202055a41caae283
Chester0629 <yfwu0629@gmail.com>
2025-11-29 03:57:23 +0800

    fix: update api base url to new backend

--- xie_vue/src/services/api.js
+++ xie_vue/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://mr-xie-s-convenience-store-ch-back-icvbux.laravel.cloud/api',
+  baseURL: 'https://mr-xie-s-convenience-store-main-d3awzd.laravel.cloud/api',
   headers: {
     Accept: 'application/json'
   }
